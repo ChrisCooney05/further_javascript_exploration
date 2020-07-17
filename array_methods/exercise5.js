de35a1faaf95ef693a4ae31386012021c136010d
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-07-17 08:44:41 +0100

    completed exercise 5 array_methods

---
+++ array_methods/exercise5.js
@@ -0,0 +1,64 @@
+// Complete the below questions using this array:
+const array = [
+  {
+    username: "john",
+    team: "red",
+    score: 5,
+    items: ["ball", "book", "pen"],
+  },
+  {
+    username: "becky",
+    team: "blue",
+    score: 10,
+    items: ["tape", "backpack", "pen"],
+  },
+  {
+    username: "susy",
+    team: "red",
+    score: 55,
+    items: ["ball", "eraser", "pen"],
+  },
+  {
+    username: "tyson",
+    team: "green",
+    score: 1,
+    items: ["book", "pen"],
+  },
+];
+
+//Create an array using forEach that has all the usernames with a "!" to each of the usernames
+const userNameArray = [];
+array.forEach((obj) => {
+  userNameArray.push(`${obj.username}!`);
+});
+
+//Create an array using map that has all the usernames with a "? to each of the usernames
+
+const userNameArray2 = array.map((obj) => `${obj.username}?`);
+
+//Filter the array to only include users who are on team: red
+const filteredArray = array.filter((obj) => obj.team === "red");
+
+//Find out the total score of all users using reduce
+const score = array.reduce((acc, num) => {
+  return acc + num.score;
+}, 0);
+//71
+
+// (1), what is the value of i?
+//index if each item
+// (2), Make this map function pure:
+const arrayNum = [1, 2, 4, 5, 8, 9];
+const newArray = arrayNum.map((num, i) => {
+  // console.log(num, i);
+  // alert(num);
+  return num * 2;
+});
+
+//BONUS: create a new list with all user information, but add "!" to the end of each items they own.
+const newUserArray = array.map((obj) => {
+  obj.item = obj.items.map((item) => {
+    return `${item}!`;
+  });
+  return obj;
+});
